204ad94d76479c8ec151388cd33e02e355d05f36
shlk69 <sahilkurmi356@gmail.com>
2024-11-18 18:02:37 +0530

    After doing sum , max and min

--- number-system/app.js
+++ number-system/app.js
@@ -9,3 +9,30 @@ for(let i = o;i<n;i++){
         console.log("not")
     }
 }  
+
+const numbers2 = [
+    19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30,
+  ];
+  let max = INT_MIN;
+  let min = INT_MAX;
+   let sum = 0;
+  let n2 = numbers.length;
+  for(let i =0;i<n2;i++){
+    sum += i;
+  }
+  console.log("sum is ",osum);
+
+  for(let i=0;i<n2;i++){
+    if(max > numbers2[i] )
+        numbers2[i] = max;
+  }
+
+  console.log("max is ",max);
+  for(let i=0;i<n2;i++){
+    if(min < numbers2[i] )
+        numbers2[i] = min;
+  }
+
+  console.log("min is ",min);
+
+  
